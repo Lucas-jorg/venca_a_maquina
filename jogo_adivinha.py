7a4a54a953325f0e41d7c5081f169e5dbb7ac238
Lucas <160195375+Lucas-jorg@users.noreply.github.com>
2025-12-11 21:51:43 -0300

    Update jogo_adivinha.py

--- jogo_adivinha.py
+++ jogo_adivinha.py
@@ -2,16 +2,6 @@
 import random as ran
 import time
 import streamlit as st
-import mysql.connector
-
-# 🔌 Conexão com MySQL
-def conectar_db():
-    return mysql.connector.connect(
-        host="SEU_HOST",       # ex: "mysql-1234.railway.app"
-        user="SEU_USUARIO",    # ex: "root"
-        password="SUA_SENHA",
-        database="jogo_web"
-    )
 
 st.title("🎮 Tenta vencer a máquina HAHA")
 st.subheader("Vamos ver se você é bom de adivinhação")
@@ -38,27 +28,6 @@ if st.button("Chutar número"):
         st.success(f"Filha da mãe, Acertooou kkkk! Eu pensei no {pc}")
     else:
         st.error(f"Se ferrou kkkk, você errou! Eu escolhi {pc} e não {palpite}")
-
-    # -------------------------------
-    #  🔥 SALVAR NO BANCO DE DADOS
-    # -------------------------------
-    try:
-        db = conectar_db()
-        cursor = db.cursor()
-
-        cursor.execute("""
-            INSERT INTO partidas (nome_jogador, resultado)
-            VALUES (%s, %s)
-        """, (nome, resultado))
-
-        db.commit()
-        cursor.close()
-        db.close()
-
-        st.info("Resultado registrado! (Você não vê, mas eu vejo 😈)")
-    
-    except Exception as e:
-        st.error(f"Erro ao conectar ao banco: {e}")
 
 
 #Site do jogo 
